cb685d65c22bb27472049b7decb638d839d25153
Chang Xin <xin.chang@autonavi.com>
2013-10-23 21:27:15 +0800

    init repo with ruby 2.0.0 + rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BjgasRepo::Application.config.secret_key_base = '37c058a08930d2938f2f475bd8609e727b73231dccf0d7f36ceefe3474000210c96eed8b6e3796cc43b33166c29b9c76742af4316c2e259511ab5e33b35009bd'
