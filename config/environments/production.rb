6f1679b9249d2d366d60b268925fd5a3c18aaa91
Chang Xin <xin.chang@autonavi.com>
2013-11-01 09:38:05 +0800

    precompile assets for production env.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,20 @@ BjgasRepo::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+
+  # Precompile additional assets
+  config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
+  config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif)
+  config.assets.precompile += %w( *.js *.css)
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
