95e9552daa973b66314535be14f5824b22838526
Chang Xin <xin.chang@autonavi.com>
2013-11-26 01:58:28 +0800

    xchang: refactored controllers.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
-class HomeController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  layout "application"
-
+class HomeController < ApplicationController
   def index
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,4 @@
-class OrdersController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
+class OrdersController < ApplicationController
   def create
     render nothing: true, status: :created
   end
