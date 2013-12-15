e421e460da874fe1dc7e80c4abd35d4db57f56b3
RenJing <reni2046@gmail.com>
2013-12-15 13:45:29 +0800

    1. create safety controller and view; 2. refactor sidebar

---
+++ app/controllers/safety_controller.rb
@@ -0,0 +1,4 @@
+class SafetyController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ BjgasRepo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
   get 'intro' => 'introduction#index'
+  get 'safety' => 'safety#index'
   get 'branch' => 'branch#index'
   resources :orders
   resources :users
